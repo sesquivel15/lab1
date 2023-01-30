5cb7c73dd500c57b85afca0c2887aea0237cabe2
Student <student@lab-walkin-003.cs.csubak.edu>
2023-01-30 11:35:23 -0800

    my first commit for lab-1

---
+++ Makefile
@@ -0,0 +1,8 @@
+all: lab1
+
+lab1: lab1.cpp
+	g++ lab1.cpp -Wall -o lab1
+
+clean:
+	rm -f lab1
+
---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+//Stephanie Esquivel
+//lab-1 Jan 24
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    //this will be used later
+    int a = rand() % 10 + 1;
+    cout << a << endl;
+    cout << "Hello World\n";
+    //lab changes 
+    cout << "Hello \n";
+
+    
+    return 0;
+}
+
